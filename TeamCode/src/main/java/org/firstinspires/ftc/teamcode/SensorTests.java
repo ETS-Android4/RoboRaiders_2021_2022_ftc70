11bec01fe223cba2c6a3c9074ea31dda20df8e55
skocik <skocik@us.ibm.com>
2021-10-13 19:26:20 -0400

    fixed sensortests

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorTests.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorTests.java
@@ -1,10 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@TeleOp(name = "Test a few sensors")
 public class SensorTests extends OpMode {
 
     ColorSensor color_sensor;
@@ -22,11 +26,13 @@ public class SensorTests extends OpMode {
     @Override
     public void loop() {
 
-        telemetry.addData("Color", color_sensor.red());
-        telemetry.addData("Color", color_sensor.blue());
-        telemetry.addData("Touch", touch_sensor.getValue());
-        telemetry.addData("Range",range_sensor.getDistance());
+        telemetry.addData("Color red: ", color_sensor.red());
+        telemetry.addData("Color blue: ", color_sensor.blue());
+        telemetry.addData("Touch: ", touch_sensor.getValue());
+        telemetry.addData("Range in mm: ",range_sensor.getDistance(DistanceUnit.MM));
+        telemetry.addData("Range in inches: ",range_sensor.getDistance(DistanceUnit.INCH));
         telemetry.update();
+
 
 
     }
